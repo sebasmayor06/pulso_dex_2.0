ec1573130fe33138c2f2a02b59cb7f48b4653c0a
yonatanlla <yonatanllanto174@gmail.com>
2024-04-04 22:27:36 -0500

    cambios beneficios

--- src/app/components/Beneficios.jsx
+++ src/app/components/Beneficios.jsx
@@ -10,8 +10,7 @@ export default function Beneficios() {
   return (
     <div ref={ref} className="overflow-x-hidden shadow-xl bg-azulInicio">
       <div className="inicio flex flex-col-reverse md:flex-row w-screen md:h-screen h-[950px] top-67px overflow-hidden left-0 bg-azulInicio md:p-16">
-      <div className="w-full md:w-1/2 h-1/2 md:h-full flex justify-center items-center relative">
-          <div className="relative">
+      <div className="w-full h-1/2 md:w-1/2 md:h-full flex justify-center items-center relative">
             <Image
               src="/celular.png"
               alt="logo"
@@ -19,8 +18,6 @@ export default function Beneficios() {
               height={500}
               className="rounded-[10%] md:w-[450px] w-[300px] md:h-[600px] h-[400px]"
             />
-            
-          </div>
         </div>
         <div className="w-full md:w-[55%] h-1/2 md:h-full flex flex-col ">
           <div className="h-1/2 flex justify-center items-center">
@@ -30,7 +27,7 @@ export default function Beneficios() {
           </div>
           <div className="h-1/2 m-3 md:m-10">
             <div className="grid grid-cols-2 gap-3">
-                    <div className={`md:h-24 md:w-72 w-44 h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[100ms] animate-normal':'hidden'} `}> 
+                    <div className={`md:h-24 w-full h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[100ms] animate-normal':'hidden'} `}> 
                     <Image 
                         src="/linea.png"
                         alt="logo"
@@ -40,7 +37,7 @@ export default function Beneficios() {
                         />
                         <span className="font-bold text-xs md:text-lg overflow-hidden">Datos y Análisis</span>
                     </div>
-                    <div className={`md:h-24 md:w-72 w-44 h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[200ms] animate-normal':'hidden'} `}> 
+                    <div className={`md:h-24 w-full h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[200ms] animate-normal':'hidden'} `}> 
                     <Image 
                         src="/estrella.png"
                         alt="logo"
@@ -50,7 +47,7 @@ export default function Beneficios() {
                         />
                         <span className="font-bold text-xs md:text-lg overflow-hidden w-40">Utilidades Semestrales</span>
                     </div>
-                    <div className={`md:h-24 md:w-72 w-44 h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[300ms] animate-normal':'hidden'} `}> 
+                    <div className={`md:h-24 w-full h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[300ms] animate-normal':'hidden'} `}> 
                     <Image 
                         src="/proh.png"
                         alt="logo"
@@ -60,7 +57,7 @@ export default function Beneficios() {
                         />
                         <span className="font-bold text-xs md:text-lg overflow-hidden">Usted no vende</span>
                     </div>
-                    <div className={`md:h-24 md:w-72 w-44 h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[400ms] animate-normal':'hidden'} `}> 
+                    <div className={`md:h-24 w-full h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[400ms] animate-normal':'hidden'} `}> 
                     <Image 
                         src="/Group.png"
                         alt="logo"
@@ -70,15 +67,7 @@ export default function Beneficios() {
                         />
                         <span className="font-bold text-xs md:text-lg overflow-hidden w-40">Contrato y Certificado</span>
                     </div>
-                    <div>
-                    <button
-              onClick={() => setShowModal2()}
-              className={`bg-black rounded-lg font-bold text-white p-2 text-sm md:text-xl hover:bg-white hover:text-black w-44 h-16 md:w-72 md:h-24 rounded-l-full rounded-r-full ${inView?'animate-jump-in animate-duration-1000 animate-delay-[500ms] animate-normal':'hidden'}`}
-            >
-              Agendar Demo
-            </button>
-                    </div>
-                    <div className={`md:h-24 md:w-72 w-44 h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[600ms] animate-normal':'hidden'} `}> 
+                    <div className={`md:h-24 w-full h-16 border-4 rounded-full border-black flex flex-row gap-3 justify-center items-center ${inView?'animate-jump-in animate-duration-1000 animate-delay-[600ms] animate-normal':'hidden'} `}> 
                     <Image 
                         src="/grafica.png"
                         alt="logo"
@@ -87,11 +76,7 @@ export default function Beneficios() {
                         className="rounded-[10%] ml-7"
                         />
                         <span className="font-bold text-xs md:text-lg overflow-hidden w-44">Acceso a informes de ventas</span>
-                    </div>
-                   
-
-                    
-                    
+                    </div>   
             </div>
           </div>
         </div>
