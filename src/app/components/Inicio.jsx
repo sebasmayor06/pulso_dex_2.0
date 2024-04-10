94052b9c3292db4559216a0966f744afe2310172
sebasmayor06 <sebda06@gmail.com>
2024-04-10 11:09:44 -0500

    update

--- src/app/components/Inicio.jsx
+++ src/app/components/Inicio.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Inicio({setShowModal2}) {
   return (
-    <div className="overflow-x-hidden shadow-xl bg-azulInicio md:p-8 md:pt-[5rem]">
+    <div className="overflow-x-hidden shadow-xl bg-azulInicio md:p-8 md:pt-[5rem] pt-12">
       <div className="inicio flex flex-col md:flex-row w-screen h-screen  top-67px overflow-hidden left-0 bg-azulInicio  gap-5 md:gap-0">
         <div className="flex flex-col justify-center items-center h-1/2 md:h-full md:w-1/2 w-full pl-4 pr-4">
           <div className="w-full h-1/2 flex justify-center items-center md:ml-14 md:mb-0 mb-10 md:mt-0 mt-10">
